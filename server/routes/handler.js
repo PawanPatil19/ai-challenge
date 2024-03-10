a650d3b3332321620c275f57ee8fc97ebb4edf7a
My Name <ishitamandal06@gmail.com>
2024-03-10 22:29:07 +0800

    setup for frontend

---
+++ server/routes/handler.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+// how to write get requests to our baackend api
+// router.get('/', (req, res) => {
+//     res.send('Hello World');
+// });
+
+//how to write post requests to our backend api
+// router.post('/', (req, res) => {
+//     console.log(req.body);
+//     res.send('Data received');
+// });
+
+
+module.exports = router; //export the router to be used in other files
